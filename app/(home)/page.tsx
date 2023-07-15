41fb340398b516facc9e9f6ec9d03684a19779c5
shinyunsung <114058540+Dev-ShinY@users.noreply.github.com>
2023-07-16 03:21:09 +0900

    :tada: Begin a project

---
+++ app/(home)/page.tsx
@@ -0,0 +1,8 @@
+import { Status } from "./status";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
+    </main>
+  );
+}
